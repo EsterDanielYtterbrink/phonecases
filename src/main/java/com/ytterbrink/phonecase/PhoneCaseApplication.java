3a0ca5be17568d9d627af3d3c0c141570d234282
Ester Daniel Ytterbrink <esterdaniel@ytterbrink.com>
2021-04-29 12:53:09 +0200

    Initial commit trusting the gitignore generated
    Used a wizard in IntelliJ to create the project

---
+++ src/main/java/com/ytterbrink/phonecase/PhoneCaseApplication.java
@@ -0,0 +1,13 @@
+package com.ytterbrink.phonecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneCaseApplication.class, args);
+    }
+
+}
