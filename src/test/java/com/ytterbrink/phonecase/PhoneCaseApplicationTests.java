b5190cb34a99b8975b62be21d2eb614f45bdc9fd
Ester Daniel Ytterbrink <esterdaniel@ytterbrink.com>
2021-05-18 10:23:06 +0200

    Can now create a phone case and get it based on another phone that has the same shape.

--- src/test/java/com/ytterbrink/phonecase/PhoneCaseApplicationTests.java
+++ src/test/java/com/ytterbrink/phonecase/PhoneCaseApplicationTests.java
@@ -29,10 +29,18 @@ class PhoneCaseApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    private ResultActions createiPhone5() throws Exception {
-        return  mockMvc.perform(post("/phones")
+    private ResultActions createPhone(String name) throws Exception {
+        return mockMvc.perform(post("/phones")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"newPhoneName\":\"iPhone5\"}}"));
+                .content("{\"newPhoneName\":\""+name+"\"}}"));
+    }
+    private ResultActions createPhone(String name, String similarPhone) throws Exception {
+        return mockMvc.perform(post("/phones")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"newPhoneName\":\""+name+"\",\"similarPhoneName\":\""+similarPhone+"\" }"));
+    }
+    private ResultActions createiPhone5() throws Exception {
+        return  createPhone("iPhone5");
     }
 
     @Test
@@ -74,7 +82,25 @@ class PhoneCaseApplicationTests {
 
     @Test
     @DirtiesContext
-    void getAllPhoneCases(){
-       // Create phone cases and get them
+    void getPhoneCaseForSimilarPhone() throws Exception {
+        createPhone("iPhone5");
+        createPhone("iPhoneSE", "iPhone5");
+        createPhone("iPhone11");
+        mockMvc.perform(post("/phoneCases")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"madeFor\":\"iPhone5\", \"name\":\"Pretty case\"}}"))
+                .andExpect(status().isCreated());
+        mockMvc.perform(post("/phoneCases")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"madeFor\":\"iPhone11\", \"name\":\"Big case\"}}"))
+                .andExpect(status().isCreated());
+        this.mockMvc.perform(get("/phoneCases"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"name\":\"Pretty case\"}, {\"name\":\"Big case\"}]", false));
+        this.mockMvc.perform(get("/phoneCases/{phone}", "iPhoneSE"))
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andExpect( content().string("[{\"id\":1,\"name\":\"Pretty case\"}]"));
     }
 }
